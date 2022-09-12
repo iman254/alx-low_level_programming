20a8950e33a27e662a61cebb425a732ac5645dac
iman254 <stephaniemarquinz@icloud.com>
2022-09-12 12:17:45 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,8 +7,8 @@
 int main(void)
 {
 	int num;
-	for (num = 0;num <= 10; num++)
-		putchar((num % 10) + '0');
+	for (num = 0; num <= 10; num++)
+		putchar(num + '0');
 	putchar('\n');
 	return (0);
 }
