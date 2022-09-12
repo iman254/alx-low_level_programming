3f50424a54390018b9128cc46cf98a728812d9af
iman254 <stephaniemarquinz@icloud.com>
2022-09-12 10:10:26 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * Main - Returns all singlgle digit numbers base 10starting from 0
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+	for (num = 0; num <10; num ++)
+		putchar("%d", num);
+	putchar("\n");
+	return (0);
+}
+
+
