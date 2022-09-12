914480b23a5ccbc151c61d22f5ab84d7c0cc9bbe
iman254 <stephaniemarquinz@icloud.com>
2022-09-12 10:29:30 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * Main - Returns all singlgle digit numbers base 10starting from 0
+ * main - Returns all singlgle digit numbers base 10starting from 0
  *
  * Return: Always 0
  */
 int main(void)
 {
 	int num;
-	for (num = 0; num <10; num ++)
+	for (num = 0; num < 10; num++)
 		printf("%d", num);
 	printf("\n");
 	return (0);
