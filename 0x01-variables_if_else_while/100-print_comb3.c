3472b6b9db72149b085683985ae140ca2a894ac5
iman254 <stephaniemarquinz@icloud.com>
2022-09-13 00:53:21 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,9 +7,9 @@
 int main(void)
 {
 	int digit1, digit2;
-	for (digit1 = 0;digit1 < 10; digit1==)
+	for (digit1 = 0; digit1 < 10; digit1++)
 	{
-		for (digit2 = 0; digit2 < 10; digit 2++)
+		for (digit2 = 0; digit2 < 10; digit2++)
 		{
 			putchar((digit1 % 10) + '0');
 			putchar((digit2 % 10) + '0');
