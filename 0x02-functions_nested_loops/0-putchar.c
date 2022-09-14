7c638c099ded0849a47e73cb048fbde2376499b7
iman254 <stephaniemarquinz@icloud.com>
2022-09-14 15:11:32 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
-#include <main.h>
+#include "main.h"
+
 /**
  * main - Prints _putchar followed by a new line
  *
@@ -6,15 +7,13 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');  
-	_putchar('u');
-	_putchar('t'); 
-	_putchar('c'); 
- 	_putchar('h'); 
-	_putchar('a'); 
-	_putchar('r');
-	_putchar('\n');
+	int i;
+	char p[] = "_putchar";
+	for (i = 0; i < 9; i++)
+		{
+		_putchar(p[i]);
+		}
+		_putchar('\n')
 	return (0);
 }	
 
