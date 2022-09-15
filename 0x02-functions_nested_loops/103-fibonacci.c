930edd603d5b82ebfb6b50a16bda7924187d7a22
iman254 <stephaniemarquinz@icloud.com>
2022-09-15 13:34:27 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints sum of the even terms in fibonacci below 4M
+ *
+ * Return: 0 on success and the print stated
+ */
+int main(void)
+{
+	int counter = 2;
+	float a = 1;
+	float b = a + 1;
+	float c = a + b;
+	printf("%.0f, ", a);
+	printf("%.0f, ", b);
+	while (counter < 98)
+	{
+		counter++;
+		printf("%.0f", c);
+		a = b;
+		b = c;
+		c = a + b;
+		if (counter < 98)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n")
+		return (0);
+}
