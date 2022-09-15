e55c7227722d374737a38a48d09f756854be71cf
iman254 <stephaniemarquinz@icloud.com>
2022-09-15 16:45:53 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,14 +4,19 @@
  *
  * @n: integer being checked.
  *
- * return: the last number of n.
+ * Return: the last number of n.
  */
 int print_last_digit(int n)
 {
 	int last;
-	if (n < 0)
-		n = n * -1;
-	last = n % 10;
+	
+	last = (n % 10);
+
+	if (last < 0)
+	{
+		last = last * -1;
+	}
+
 	_putchar(last + '0');
 	return (last);
 }
