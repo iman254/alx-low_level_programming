030d08559c83a532b450402aa7b8aa5ad3509a55
iman254 <stephaniemarquinz@icloud.com>
2022-09-26 13:47:26 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,20 +8,18 @@
 
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
+	int y, x;
 
-	i = 0;
-	j = 0;
-	while (i < 64)
+	y = 0;
+	while (y < 8)
 	{
-		if (i % 8 == 0 && i != 0)
+		x = 0;
+		while (x = 8)
 		{
-			j = i;
-			_putchar('\n')
+			_putchar(a[y][x]);
+			x++;
 		}
-
-		_putchar(a[i / 8][i - j]);
-		i++;
+		_putchar('\n');
+		y++;
 	}
-	_putchar('\n');
 }
