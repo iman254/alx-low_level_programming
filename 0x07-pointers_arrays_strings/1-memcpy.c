bae18c0e5207254cc7b8d579d024782d7d79de84
iman254 <stephaniemarquinz@icloud.com>
2022-09-26 12:22:20 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,14 +5,14 @@
  * @dest: buffer where we will copy to
  * @src: what we are to copy
  * @n: n bytes of @src
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
-
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < n; i++;)
+	for (i = 0; i < n; i++)
 		dest[i] = src[i];
 	return (dest);
 }
+ 
