65d5578b64443efab600802422db6b58610a7a0b
iman254 <stephaniemarquinz@icloud.com>
2022-09-28 01:47:11 -0700

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -14,14 +14,15 @@
 int _pow_recursion(int x, int y)
 {
 	int even;
-	if (y > 0)
+
+	if (y < 0)
 		return (-1);
 	else if (y == 0)
 		return (1);
 	else if (y % 2 == 0)
 	{
 		even = _pow_recursion(x, y / 2);
-		return(even * even);
+		return (even * even);
 	}
 	else
 		return (x * _pow_recursion(x, y - 1));
