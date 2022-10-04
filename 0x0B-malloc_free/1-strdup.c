fd651b17717c436853ae1e1bdfe802d1a777a542
iman254 <stephaniemarquinz@icloud.com>
2022-10-04 03:52:14 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,22 +1,36 @@
-#include <stdlib.h>
 #include "main.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - check the code for ALX School students.
- * Return: Always 0.
+ *
+ * _strdup - Duplicate a string using malloc
+ *
+ * @str: string to duplicate
+ *
+ * Return: Pointer to a the new duped string
+ *
  */
-int main(void)
-{
-	char *s;
 
-	s = _strdup("ALX SE");
-	if (s == NULL)
-	{
-		printf("failed to allocate memory\n");
-		return (1);
-	}
-	printf("%s\n", s);
-	free(s);
-	return (0);
+char *_strdup(char *str)
+{
+	char *a;
+	int i, c;
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; sre[i] != '\0'; i++)
+		;
+
+	a = malloc(i * sizeof(*a) + 1);
+	if (a == NULL)
+		return (NULL);
+
+
+	for (c = 0; c < i; c++)
+		a[c] = str[c];
+	a[c] = '\0';
+
+	return (a);
 }
+
+
